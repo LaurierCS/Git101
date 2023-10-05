4c2ee90c85d7a91431ad97dcad2541770564ad98
Nausher Rao <SherRao15@gmail.com>
2023-10-05 19:54:12 -0400

    Add a new isEven function

---
+++ src/main.py
@@ -0,0 +1,39 @@
+def isEven(number: int):
+    if(number == 0):
+        return True
+
+    elif(number == 1):
+        return False
+
+    elif(number == 2):
+        return True
+
+    elif(number == 3):
+        return False
+
+    elif(number == 4):
+        return True
+
+    elif(number == 5):
+        return False
+
+    elif(number == 6):
+        return True
+
+    elif(number == 7):
+        return False
+
+    elif(number == 8):
+        return True
+
+    elif(number == 9):
+        return False
+
+    elif(number == 10):
+        return True
+
+    elif(number == 11):
+        return False
+
+    else:
+        raise Exception("Number is too big")
